4648b404012772f58953690210be1ac7e108f0b7
Shahriar Fardows <shahriarfardows@gmail.com>
2024-08-21 21:46:27 +0600

    site map and robots file add

--- src/Components/Contact/Info.jsx
+++ src/Components/Contact/Info.jsx
@@ -4,89 +4,91 @@ const Info = () => {
         <div className="container mx-auto py-20">
             <div>
                 <h1 className="mb-4 px-5 text-4xl lg:text-6xl tracking-tight font-play  text-lemon-20 dark:text-white ">Let&apos;s Work Together</h1>
-                <div className="flex justify-around items-center py-10">
-                    <div className="px-5">
-                        <h5 className="pt-3">My Address:</h5><br />
-                        <h3 className="pb-3 text-lemon-20 font-bold font-play text-2xl md:text-3xl lg:text-4xl">
-                            Basaboo, Kalibari, Dhaka-1214, Bangladesh
-                        </h3>
-                        <hr />
-                        <h5 className="pt-3">Call me:</h5><br />
-                        <h3 className="pb-3 text-lemon-20 font-bold font-play text-2xl md:text-3xl lg:text-4xl">
-                            +8801917621525
-                        </h3>
-                        <hr />
-                        <h5 className="pt-3">Mail me:</h5><br />
-                        <h3 className="pb-3 text-lemon-20 font-bold font-play text-2xl md:text-3xl lg:text-4xl">
-                            <span>shahriarfardows@gmail.com</span>
-                        </h3>
-                        <hr />
+                <div className="grid grid-cols-1 lg:grid-cols-2 justify-around items-center ">
+                    <div className="py-10">
+                        <div className="px-5">
+                            <h5 className="pt-3">My Address:</h5><br />
+                            <h3 className="pb-3 text-lemon-20 font-bold font-play text-2xl md:text-3xl lg:text-4xl">
+                            Kachari Para, Dakshin Gao, Noya Para Rd, Dhaka 1214
+                            </h3>
+                            <hr />
+                            <h5 className="pt-3">Call me:</h5><br />
+                            <h3 className="pb-3 text-lemon-20 font-bold font-play text-2xl md:text-3xl lg:text-4xl">
+                                +8801917621525
+                            </h3>
+                            <hr />
+                            <h5 className="pt-3">Mail me:</h5><br />
+                            <h3 className="pb-3 text-lemon-20 font-bold font-play text-2xl md:text-3xl lg:text-4xl">
+                                <span>shahriarfardows@gmail.com</span>
+                            </h3>
+                            <hr />
+                        </div>
                     </div>
-                </div>
-                <section className="bg-white dark:bg-gray-900">
-                    <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-                        <p className="mb-4 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
-                            To get in touch with us, please fill out this form. We will get back to you shortly.
-                        </p>
-                        <form action="#" className="space-y-2">
-                            <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
+                    <section className="bg-white dark:bg-gray-900">
+                        <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+                            <p className="mb-4 font-light text-gray-500 dark:text-gray-400 sm:text-xl">
+                                To get in touch with us, please fill out this form. We will get back to you shortly.
+                            </p>
+                            <form action="#" className="space-y-2">
+                                <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
+                                    <div>
+                                        <label htmlFor="Name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                                            Your Name
+                                        </label>
+                                        <input
+                                            type="text"
+                                            id="Name"
+                                            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                                            placeholder="name@flowbite.com"
+                                            required
+                                        />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                                            Your email
+                                        </label>
+                                        <input
+                                            type="email"
+                                            id="email"
+                                            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                                            placeholder="name@flowbite.com"
+                                            required
+                                        />
+                                    </div>
+                                </div>
                                 <div>
-                                    <label htmlFor="Name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                                        Your Name
+                                    <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                                        Subject
                                     </label>
                                     <input
                                         type="text"
-                                        id="Name"
-                                        className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                                        placeholder="name@flowbite.com"
+                                        id="subject"
+                                        className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                                        placeholder="Let us know how we can help you"
                                         required
                                     />
                                 </div>
-                                <div>
-                                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                                        Your email
+                                <div className="sm:col-span-2">
+                                    <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">
+                                        Your message
                                     </label>
-                                    <input
-                                        type="email"
-                                        id="email"
-                                        className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                                        placeholder="name@flowbite.com"
-                                        required
-                                    />
+                                    <textarea
+                                        id="message"
+                                        rows="6"
+                                        className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                        placeholder="Leave a comment..."
+                                    ></textarea>
                                 </div>
-                            </div>
-                            <div>
-                                <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                                    Subject
-                                </label>
-                                <input
-                                    type="text"
-                                    id="subject"
-                                    className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                                    placeholder="Let us know how we can help you"
-                                    required
-                                />
-                            </div>
-                            <div className="sm:col-span-2">
-                                <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">
-                                    Your message
-                                </label>
-                                <textarea
-                                    id="message"
-                                    rows="6"
-                                    className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                    placeholder="Leave a comment..."
-                                ></textarea>
-                            </div>
-                            <button
-                                type="submit"
-                                className="inline-flex h-10 items-center justify-center gap-2 whitespace-nowrap rounded bg-hello-10 px-5 text-sm font-medium tracking-wide text-white shadow-md shadow-hello-10 transition duration-300 hover:bg-lemon-20 hover:shadow-sm hover:shadow-hello-10 focus:bg-hello-10 focus:shadow-sm focus:shadow-lemon-20 focus-visible:outline-none disabled:cursor-not-allowed disabled:border-lemon-20 disabled:bg-lemon-20 disabled:shadow-none"
-                            >
-                                <span className='font-play'> Send message</span>
-                            </button>
-                        </form>
-                    </div>
-                </section>
+                                <button
+                                    type="submit"
+                                    className="inline-flex h-10 items-center justify-center gap-2 whitespace-nowrap rounded bg-hello-10 px-5 text-sm font-medium tracking-wide text-white shadow-md shadow-hello-10 transition duration-300 hover:bg-lemon-20 hover:shadow-sm hover:shadow-hello-10 focus:bg-hello-10 focus:shadow-sm focus:shadow-lemon-20 focus-visible:outline-none disabled:cursor-not-allowed disabled:border-lemon-20 disabled:bg-lemon-20 disabled:shadow-none"
+                                >
+                                    <span className='font-play'> Send message</span>
+                                </button>
+                            </form>
+                        </div>
+                    </section>
+                </div>
             </div>
 
         </div>
--- src/Components/Contact/Map.jsx
+++ src/Components/Contact/Map.jsx
@@ -1,10 +1,29 @@
 
 const Map = () => {
     return (
-        <div>
-            
-        </div>
+        <section className="text-gray-600 body-font relative">
+            <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
+                <div className="lg:w-full md:w-1/2 h-[50vh] bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+                    <iframe width="100%" height="100%" className="absolute inset-0" frameBorder="0" title="map" marginHeight="0" marginWidth="0" scrolling="no" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d913.0610753017829!2d90.44485661208113!3d23.7386660970444!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b81f5efb5485%3A0xb04fa4f1d0446414!2z4Kas4Ka-4Kav4Ka84Kak4KeB4KayIOCmruCmvifgpq7gp4HgprAg4Kac4Ka-4Kau4KeHIOCmruCmuOCmnOCmv-Cmpg!5e0!3m2!1sbn!2sbd!4v1724254804770!5m2!1sbn!2sbd"
+                    ></iframe>
+                    <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
+                        <div className="lg:w-1/2 px-6">
+                            <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">ADDRESS</h2>
+                            <p className="mt-1">PCQW+JJP, Kachari Para, Dakshin Gao, Noya Para Rd, ঢাকা 1214</p>
+                        </div>
+                        <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+                            <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">EMAIL</h2>
+                            <a href="mailto:shahriarfardows@gmail.com" className="text-indigo-500 leading-relaxed">shahriarfardows@gmail.com</a>
+                            <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">PHONE</h2>
+                            <p className="leading-relaxed">+8801917621525</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
 export default Map;
+
+
