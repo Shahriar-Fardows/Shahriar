7f52aa4fa357d912f5cf22ecd5fe127619066c43
Shahriar Fardows <shahriarfardows@gmail.com>
2024-10-03 11:08:13 +0600

    solve problem

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -75,7 +75,9 @@ const Footer = () => {
                     <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
                         © 2024{' '}
                         
+                            <Link to='https://github.com/Shahriar-Fardows/component/tree/main'>
                             Shahriar™
+                            </Link>
                         
                         . All Rights Reserved.
                     </span>
